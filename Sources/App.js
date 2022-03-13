ff5e649cab37056363934129851362888fef069b
Russell-AW <81236404+Russell-AW@users.noreply.github.com>
2022-03-13 14:54:08 +0700

    Add files via upload

---
+++ Sources/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Styles.css';
+import ListGoesOn from './Compos/ListGoesOn';
+
+function App() {
+  return (
+    <div className="TODO">
+      <ListGoesOn />
+    </div>
+  );
+}
+
+export default App;
